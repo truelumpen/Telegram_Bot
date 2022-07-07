1c3de2c7567fb6b3418c1b9c7f6b70ab326f5f89
ategran <3zes4u@gmail.com>
2022-07-07 11:40:47 +0300

    delete space in uri

--- config_file.py
+++ config_file.py
@@ -1,7 +1,7 @@
 telegram_token = '5422591657:AAFUCACE7p2FQU8bCfyNlpX5yeKzwwfoFxc'  # Бота создавать через @Botfather
 admin_id = 950239673
 URI = 'postgres://ainsadounkcwfb:de58e0fe950c1feef5bb11bf67baed160bd583ab5a1eff4f6e333d448b43a032@ec2-52-30-67-143.eu' \
-      '-west-1.compute.amazonaws.com:5432/dedl2u341j47dt '
+      '-west-1.compute.amazonaws.com:5432/dedl2u341j47dt'
 start_text = '''
     This is a multifunctional bot \t
     It's a part of my portfolio \t
